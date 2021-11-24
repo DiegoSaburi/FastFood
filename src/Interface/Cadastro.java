63fcd885d731ad47c3ebd91de652673dc65d40ff
DiegoSaburi <diego_saburi@hotmail.com>
2021-11-23 21:11:52 -0300

    Patch: Adicionada interfaces grafica da página de cadastro dos guiches.

---
+++ src/Interface/Cadastro.java
@@ -0,0 +1,53 @@
+package Interface;
+
+import Utils.Input;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Cadastro extends JFrame implements CoresTitanicas, FontesTitanicas{
+	private JPanel cadastro;
+	private JPanel container;
+	private JLabel quantidadeGuiches;
+	private JTextField inputQuantidadeGuiches;
+	private JButton ok;
+
+	public Cadastro(Input input) throws HeadlessException {
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setContentPane(cadastro);
+		this.setLocationRelativeTo(null);
+		this.setSize(640,480);
+		this.pack();
+		quantidadeGuiches.setFont(fonte16);
+		cadastro.setBackground(CoresTitanicas.amareloEscuro);
+		container.setBackground(CoresTitanicas.amareloEscuro);
+		this.setBackground(CoresTitanicas.amareloEscuro);
+		ok.setBackground(CoresTitanicas.cinza);
+		ok.setForeground(CoresTitanicas.amareloEscuro);
+		ok.setFont(fonte16);
+		ok.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String inputUser = inputQuantidadeGuiches.getText();
+				if(!Input.isInt(inputUser)){
+					JOptionPane.showMessageDialog(cadastro,
+							"Entre com um numero inteiro!",
+							"error",
+							JOptionPane.ERROR_MESSAGE);
+				}
+				else if(!Input.setQuantidadeGuiches(Integer.parseInt(inputUser))){
+					JOptionPane.showMessageDialog(cadastro,
+							"Entre com um numero maior que 0!",
+							"error",
+							JOptionPane.ERROR_MESSAGE);
+				}else{
+					Input.setQuantidadeGuiches(Integer.parseInt(inputUser));
+				}
+
+			}
+		});
+	}
+}

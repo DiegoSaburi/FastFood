d81e218c5e1d55eeb24c6d189ba235f9bde180da
DiegoSaburi <diego_saburi@hotmail.com>
2021-11-23 21:07:48 -0300

    Patch: Adicionadas interfaces para lidar com pallet da empresa.

---
+++ src/Interface/CoresTitanicas.java
@@ -0,0 +1,13 @@
+package Interface;
+
+import java.awt.*;
+
+/**
+ * Contém as cores utilizadas pela TITAN
+ */
+public interface CoresTitanicas{
+	Color amareloEscuro = new Color(242,169,0);
+	Color amareloClaro = new Color(253,187,34);
+	Color cinza = new Color(66,66,66);
+	Color azul = new Color(55,55,79);
+}
